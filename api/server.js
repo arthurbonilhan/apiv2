9c8a252d7c9ada463f686802c409dc1ddf590df3
Arthur Bonilha <32990047+arthurbonilhan@users.noreply.github.com>
2024-05-22 02:21:54 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,37 +1,39 @@
-const jsonServer = require('json-server')
-const fs = require('fs')
-const path = require('path')
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
 // Crie o diretório /tmp se ele ainda não existir
-const tmpDir = './tmp'
+const tmpDir = './tmp';
 if (!fs.existsSync(tmpDir)) {
-  fs.mkdirSync(tmpDir)
+  fs.mkdirSync(tmpDir);
 }
 
 // Defina o caminho do arquivo de banco de dados temporário
-const tmpDbFile = path.join(tmpDir, 'db.json')
+const tmpDbFile = path.join(tmpDir, 'db.json');
 
 // Defina o roteador usando o novo caminho do banco de dados temporário
-const router = jsonServer.router(tmpDbFile)
+const router = jsonServer.router(tmpDbFile);
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id',
   })
-)
+);
 
-server.use(router)
+server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// Use process.env.PORT para determinar a porta de escuta, se estiver definida, caso contrário, use a porta 3000
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
 
 // Exporte o servidor API
-module.exports = server
+module.exports = server;
