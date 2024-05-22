6a2d28acc245f73185300386caee9c8484f87248
Arthur Bonilha <32990047+arthurbonilhan@users.noreply.github.com>
2024-05-22 02:16:15 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,37 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const fs = require('fs')
+const path = require('path')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// Crie o diretório /tmp se ele ainda não existir
+const tmpDir = './tmp'
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir)
+}
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// Defina o caminho do arquivo de banco de dados temporário
+const tmpDbFile = path.join(tmpDir, 'db.json')
+
+// Defina o roteador usando o novo caminho do banco de dados temporário
+const router = jsonServer.router(tmpDbFile)
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
+
+server.use(
+  jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+    '/blog/:resource/:id/show': '/:resource/:id',
+  })
+)
+
 server.use(router)
+
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running')
 })
 
-// Export the Server API
+// Exporte o servidor API
 module.exports = server
