32216794a2dd474b84c64f15a038a8dcc5eaa87a
Arthur Bonilha <32990047+arthurbonilhan@users.noreply.github.com>
2024-05-22 02:25:32 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,39 +1,31 @@
-const jsonServer = require('json-server');
-const fs = require('fs');
-const path = require('path');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
+const server = jsonServer.create()
 
-// Crie o diretório /tmp se ele ainda não existir
-const tmpDir = './tmp';
-if (!fs.existsSync(tmpDir)) {
-  fs.mkdirSync(tmpDir);
-}
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-// Defina o caminho do arquivo de banco de dados temporário
-const tmpDbFile = path.join(tmpDir, 'db.json');
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
 
-// Defina o roteador usando o novo caminho do banco de dados temporário
-const router = jsonServer.router(tmpDbFile);
+const middlewares = jsonServer.defaults()
 
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-
-server.use(
-  jsonServer.rewriter({
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id',
-  })
-);
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-server.use(router);
-
-// Use process.env.PORT para determinar a porta de escuta, se estiver definida, caso contrário, use a porta 3000
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
-
-// Exporte o servidor API
-module.exports = server;
+// Export the Server API
+module.exports = server
